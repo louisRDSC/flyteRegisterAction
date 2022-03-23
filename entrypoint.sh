d7b7e73287d2cb03155e65f7c284220069cdb956
lrdsc <louis.rousselot-de-saint-ceran@artefact.com>
2022-03-23 14:31:11 +0100

    feature/firstDraft

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir secret
 touch secret/secret
-echo $7 > /secret/secret
+echo $7 > secret/secret
 
 flytectl -c $1 register files --project $2 --domain $3 --archive $4 --force --version $5 --admin.clientId $6 --admin.clientSecretLocation secret/secret
